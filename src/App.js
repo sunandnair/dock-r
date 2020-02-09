9ebb7dfdbf3f50ce830290d4a6f78fa36d66c2e5
Sunand Nair <sunand.nair@outlook.com>
2020-02-09 12:12:51 +0000

    app change - port

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload!!!.
         </p>
-        <h1>Isn't that fantastico. Enthonnu!?!!</h1>
+        <h1>Allallenthu kathayithu kashtame! Enthonnu!?!!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
